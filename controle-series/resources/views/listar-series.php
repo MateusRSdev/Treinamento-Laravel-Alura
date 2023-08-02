a6f26ffc40cd4438747e2a6c381653d9a1516bac
MateusRSdev <mateusRSdev@gmail.com>
2023-08-01 21:18:46 -0300

    aula 3

---
+++ controle-series/resources/views/listar-series.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>series</title>
+</head>
+<body>
+    <h1>series</h1>
+
+    <ul>
+
+    <?php foreach($series as $serie){?>
+        <li><?= $serie ?></li>
+    <?php }?>
+
+    </ul>
+</body>
+</html>
