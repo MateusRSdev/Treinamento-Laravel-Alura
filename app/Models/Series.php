549a8bb5c3020ad6a0d782dcb9f8efa8d477780a
MateusRSdev <mateusRSdev@gmail.com>
2023-08-10 08:50:30 -0300

    c2 a5

---
+++ app/Models/Series.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Serie extends Model
+{
+    use HasFactory;
+    protected $fillable = ['nome'];
+    // protected $with = ["temporadas"];
+
+    public function season(){
+        return $this->hasMany(Season::class,"series_id");
+    }
+    public static function booted(){
+        self::addGlobalScope("ordered", function (Builder $queryBuilder) {
+            $queryBuilder->orderBy("nome");
+        });
+    }
+}
