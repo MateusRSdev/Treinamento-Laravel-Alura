185d672e3286b43de8949dc28151bebf0329311f
MateusRSdev <mateusRSdev@gmail.com>
2023-08-14 09:31:39 -0300

    sobrevivendo a uma aula de injecao de dependencia

--- app/Http/Controllers/SeasonsController.php
+++ app/Http/Controllers/SeasonsController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Serie;
+use App\Models\Series;
 use Illuminate\Http\Request;
 
 class SeasonsController extends Controller
 {
-    public function index(Serie $serie){
+    public function index(Series $serie){
         $seasons = $serie->seasons()->with("episodes")->get();
         // dd($seasons->all());
         return view("seasons.index")->with("seasons",$seasons)->with("series",$serie);
--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -3,18 +3,21 @@
 namespace App\Http\Controllers;
 
 
-use App\Models\Serie;
-use App\Models\Season;
-use App\Models\Episode;
+use App\Models\Series;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+use App\Repositories\SeriesRepository;
 use App\Http\Requests\SeriesFormRequest;
+use App\Repositories\EloquentSeriesRepository;
 
 class SeriesController extends Controller
 {
+
+    public function __construct(private SeriesRepository $seriesRepository) {
+    
+    } 
     public function index(Request $request)
     {
-        $series = Serie::with(["seasons"])->get();
+        $series = Series::with(["seasons"])->get();
         $mensagemSucesso = session('mensagem.sucesso');
 
         return view('series.index')->with('series', $series)
@@ -28,45 +31,13 @@ class SeriesController extends Controller
 
     public function store(SeriesFormRequest $request)
     {
+        $serie = $this->seriesRepository->add($request);
+        return to_route('series.index')
+            ->with('mensagem.sucesso', "Série '{$serie->nome}' adicionada com sucesso");
 
-        try {
-            $serie = DB::transaction(function () use ($request, &$serie) {
-            $serie = Serie::create($request->all());
-            $seasons = [];
-
-            for ($i = 1; $i <= $request->seasonsQtd; $i++) {
-                $seasons[] = [
-                    "series_id" => $serie->id,
-                    "number" => $i
-                ];
-
-            }
-            Season::insert($seasons);
-
-            $episodes = [];
-            foreach ($serie->seasons as $season) {
-                for ($j = 1; $j <= $request->episodesPerSeason; $j++) {
-                    $episodes[] = [
-                        "season_id" => $season->id,
-                        "number" => $j
-                    ];
-                }
-            }
-            Episode::insert($episodes);
-
-            return $serie;
-
-            }, 5);
-
-            return to_route('series.index')
-                ->with('mensagem.sucesso', "Série '{$serie->nome}' adicionada com sucesso");
-
-        }catch(\Throwable $e){
-            dd($e);
-        }
     }
 
-    public function destroy(Serie $series)
+    public function destroy(Series $series)
     {
         $series->delete();
 
@@ -74,12 +45,12 @@ class SeriesController extends Controller
             ->with('mensagem.sucesso', "Série '{$series->nome}' removida com sucesso");
     }
 
-    public function edit(Serie $series)
+    public function edit(Series $series)
     {
         return view('series.edit')->with('serie', $series);
     }
 
-    public function update(Serie $series, SeriesFormRequest $request)
+    public function update(Series $series, SeriesFormRequest $request)
     {
         $series->fill($request->all());
         $series->save();
