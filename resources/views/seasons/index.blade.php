439e58f9870c47ba3915973a3f0e738dbfb29a6a
MateusRSdev <mateusRSdev@gmail.com>
2023-08-23 09:33:45 -0300

    aprendendo upload de arquivos

--- resources/views/seasons/index.blade.php
+++ resources/views/seasons/index.blade.php
@@ -1,4 +1,5 @@
 <x-layout title="Temporadas de {!! $series->nome !!}">
+    <img src="{{ asset("storage/". $series->cover)}}" class="img-thumbnail rounded m-3" style="height: 400px" alt="capa da serie">
     <ul class="list-group">
         @foreach ($seasons as $season)
             <li class="list-group-item d-flex justify-content-between align-items-center">
